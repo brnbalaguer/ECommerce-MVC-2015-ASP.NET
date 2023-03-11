e39a7553ef5133245ba7f87cb90b1f74fcdfa2cd
Bruno <Bruno@PC-SALA>
2023-03-11 12:53:07 -0300

    Add project files.

---
+++ ECommerce/Models/Departments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Departments
+    {
+
+        [Key]
+        public int DepartmentsId { get; set; }
+
+        [Required(ErrorMessage =("O Campo Nome é obrigatorio!"))]
+        public string Name { get; set; }
+    }
+}
